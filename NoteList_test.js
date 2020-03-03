eb2a7cc3b2da4f259cba7c3a2c4d9762bead18d7
Peter <pc.dean@icloud.com>
2020-03-03 16:03:50 +0000

    remove redundent tests

--- NoteList_test.js
+++ NoteList_test.js
@@ -1,9 +1,5 @@
 var list = require('./NoteList.js');
 
-if (typeof list === 'object') {
-  console.log('pass')}
-else {console.log('fail')};
-
 if (typeof list.viewNote === 'function') {
   console.log('pass')}
 else {console.log('fail')};
--- Note_test.js
+++ Note_test.js
@@ -1,9 +1,5 @@
 var note = require('./Note.js')
 
-if (typeof note === 'object') {
-  console.log('pass')}
-else {console.log('fail')};
-
 if (typeof note.first20 === 'function') {
   console.log('pass')}
 else {console.log('fail')};
