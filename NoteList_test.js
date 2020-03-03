12c4c120b7901d47e1a61408d7cc6bc2fe070e05
Peter <pc.dean@icloud.com>
2020-03-03 21:37:28 +0000

    remove redundannt tests

--- NoteList_test.js
+++ NoteList_test.js
@@ -4,8 +4,6 @@ list.addNote('test', 'this is a test note');
 
 isWorking('addNote method should add note to list', list.viewNotes() == 'test')
 
-isWorking('viewNote method exists', typeof list.viewNotes === 'function')
-
 // Conditional Comparison Method
 function isWorking(testName, condition) {
   if (condition) {console.log(testName + ': ' + 'pass')}
